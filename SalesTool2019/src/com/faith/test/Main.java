d25883bd6cb3c7ce87faa2bd5b9467ad72f7e69f
user <user@AbhiJ>
2019-09-23 15:44:10 +0530

    second commit to git

--- SalesTool2019/src/com/faith/test/Main.java
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -3,7 +3,7 @@ package com.faith.test;
 public class Main {
 	public static void main(String[] args){
 		SalesData objSalesData=new SalesData();
-		System.out.println("Hello");
+		System.out.println("Hello UST");
 		displayGreetings();
 		objSalesData.display();
 	}
--- SalesTool2019/src/com/faith/test/SalesData.java
+++ SalesTool2019/src/com/faith/test/SalesData.java
@@ -3,6 +3,6 @@ package com.faith.test;
 public class SalesData {
 	void display()
 	{
-		System.out.println("hai");
+		System.out.println("sales report 2019");
 	}
 }
