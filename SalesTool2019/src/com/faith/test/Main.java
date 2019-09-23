44917f92b33319230df06864609b4405c4e62493
user <user@AbhiJ>
2019-09-23 15:54:19 +0530

    Third commit

--- SalesTool2019/src/com/faith/test/Main.java
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -3,9 +3,11 @@ package com.faith.test;
 public class Main {
 	public static void main(String[] args){
 		SalesData objSalesData=new SalesData();
+		Third objSalesData1=new Third();
 		System.out.println("Hello UST");
 		displayGreetings();
 		objSalesData.display();
+		objSalesData1.print();
 	}
 	private static void displayGreetings(){
 		System.out.println("Welcome to sales report");
---
+++ SalesTool2019/src/com/faith/test/Third.java
@@ -0,0 +1,8 @@
+package com.faith.test;
+
+public class Third {
+	void print()
+	{
+		System.out.println("HELLO WORLD");
+	}
+}
